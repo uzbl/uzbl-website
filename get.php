f2d000a03c0f933634ecf91839973df14168c79b
Dieter Plaetinck <dieter@plaetinck.be>
2009-05-01 14:07:16 +0200

    support reading files (INSTAL file) from uzbl source clone using markdown syntax. no need to maintain html page

--- get.php
+++ get.php
@@ -26,6 +26,7 @@
       <div id="navigation">
         <ul>
           <li><a href="/bugs/">Bugs</a></li>
+          <li><a href="/faq.php">Faq</a></li>
           <li id="selected"><a href="/get.php">Get</a></li>
           <li><a href="/contribute.php">Contribute</a></li>
           <li><a href="/">Home</a></li>
@@ -33,41 +34,9 @@
       </div>
       
       <div id="main">
-        <h2>Arch Users</h2>
-        <p>If you use Arch Linux (our distro of choice, and the distro we use for testing), you can find a PKGBUILD on the AUR <a href="http://aur.archlinux.org/packages.php?ID=25972">here</a>, which installs the latest from the master branch. You can edit the PKGBUILD to change to any other branch you want.</p>
-
-        <h2>Everyone Else</h2>
-        <p>If you don't use Arch, or if you do and don't want to use the PKGBUILD for some strange reason, you can use the following lines in your terminal (make sure you have git, Gtk2, and libwebkit installed):</p>
-        <pre><code>$ git clone git://github.com/Dieterbe/uzbl.git
-$ cd uzbl
-$ make
-$ make install</code></pre>
-<h2>After installing</h2>
-  <p>You will have the program in /usr/bin and various sample scripts, a sample config, sample bookmarks file and some documentation in /usr/share/uzbl.  You will probably want to change the scripts to behave more like you want, so copy the scripts to your home dir.
-  If you save your config as $XDG_CONFIG_HOME/uzbl/config (this expands to ~/.config/uzbl/config on most systems) it will be recognized automatically.  You can also pass the path to the config file with the --config parameter.</p>
-
-        <h3>Branches</h3>
-        <p>The branches are listed on the home page, but just so you don't have to go back to have a look, here they are again:</p>
-        <div id="branches">
-          <ul>
-            <li>Dieterbe
-              <ol>
-                <li><a href="http://github.com/Dieterbe/uzbl/tree/master">master</a></li>
-                <li><a href="http://github.com/Dieterbe/uzbl/tree/experimental">experimental</a></li>
-            </ol></li>
-            <li>dusanx
-              <ol>
-                <li><a href="http://github.com/dusanx/uzbl/tree/master">master</a></li>
-            </ol></li>
-            <li>Barrucadu
-              <ol>
-                <li><a href="http://github.com/Barrucadu/uzbl/tree/experimental">experimental</a></li>
-            </ol></li>
-          </ul>
-        </div>
-        
-        <h3>Note</h3>
-        <p>As we do all of our development and testing on Arch, you may encounter bugs we can't reproduce easily. In that case, make sure to provide as much information as you can.</p>
+      <?php
+        require_once 'markdown-1.0.1m/markdown.php';
+        echo Markdown(file_get_contents('../uzbl/INSTALLING'));?>
       </div>
     </div>
 <script type="text/javascript">
