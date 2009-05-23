301e79da79d2a7890409bbe76f705d5a3719b8cc
Barrucadu <mike@barrucadu.co.uk>
2009-05-23 20:36:17 +0100

    Fixed XHTML warning

--- templates/layout.php
+++ templates/layout.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
   <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta name="keywords" content="uzbl, browser, open source"/>
 	<meta name="description" content="Uzbl is a lightweight webkit browser following the UNIX philosophy - to do one thing and do it well."/>
 	<meta name="author" content="Michael Walker"/>
 	<meta name="robots" content="FOLLOW,INDEX"/>
-
+   
 	<title>Uzbl - the uzbl browser.</title>
 
 	<link rel="stylesheet" href="/style.css" type="text/css" />
