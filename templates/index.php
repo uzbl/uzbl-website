a3fb86d214915c33c0ee75b5077ac3bb919f3a9b
Dieter Plaetinck <dieter@plaetinck.be>
2009-06-01 18:14:02 +0200

    use list instead of paragraphs

--- templates/index.php
+++ templates/index.php
@@ -45,11 +45,14 @@
   <div id="about">
     <h2>The uzbl browser</h2>
     <p>Uzbl follows the UNIX philosophy - &quot;Write programs that do one thing and do it well. Write programs to work together. Write programs to handle text streams, because that is a universal interface.&quot;</p>
-    <p>very minimal graphical interface. You only see what you need</p>
-    <p>what is not browsing, is not in uzbl.  Things like url changing, loading/saving of bookmarks, saving history, downloads, ... are handled through <b>external</b> scripts that you write</p>
-    <p>controllable through various means such as fifo and socket files, stdin, keyboard and more</p>
-    <p>advanced, customizable keyboard interface with support for modes, modkeys, multichars, keywords, variable expansion etc. (if you don't understand this: it means you can tweak the interface to be vim-like, emacs-like or any-other-program-like)</p>
-    <p>Uzbl keeps it simple, and puts <b>you</b> in charge.</p>
+    <ul>
+    <li>very minimal graphical interface. You only see what you need</li>
+    <li>what is not browsing, is not in uzbl.  Things like url changing, loading/saving of bookmarks, saving history, downloads, ... are handled through <b>external</b> scripts that you write</li>
+    <li>controllable through various means such as fifo and socket files, stdin, keyboard and more</li>
+    <li>advanced, customizable keyboard interface with support for modes, modkeys, multichars, keywords, variable expansion etc. (if you don't understand this: it means you can tweak the interface to be vim-like, emacs-like or any-other-program-like)</li>
+    <li>focus on plaintext storage for your data and configs in simple, parseable formats</li>
+    <li>Uzbl keeps it simple, and puts <b>you</b> in charge.</li>
+    </ul>
     <p>Uzbl is under heavy development.  No release has been made so far.  See the <a href="/get.php">Get uzbl</a> page</p>
   </div>
   
