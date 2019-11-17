0b8bfba810aa4fb19298299606acc10da4b1a8b5
Michael Walker <mike@barrucadu.co.uk>
2019-11-17 21:27:41 +0000

    Remove GA javascript
    God knows who has access to the account that was logging to, and it was broken by the content security policy anyway.

--- templates/layout.php
+++ templates/layout.php
@@ -25,14 +25,5 @@
 <?php echo $CONTENT_FOR_LAYOUT ?>
 
     </div>
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-4514522-11");
-pageTracker._trackPageview();
-} catch(err) {}</script>
   </body>
 </html>
